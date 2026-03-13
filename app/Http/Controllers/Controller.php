c1b59e2656f9fc2f538912e01eba62cf1c7374c9
Roman <r.sharapov@stream-telecom.ru>
2026-03-13 20:06:34 +0300

    feat(Contoller): add responseOk and responseFail

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,28 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
+
+    public function responseOk($data, $message = 'All good')
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'data' => $data,
+        ]);
+    }
+
+    public function responseFail($message = 'All bad', $code = 400)
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message,
+        ], $code);
+    }
 }
