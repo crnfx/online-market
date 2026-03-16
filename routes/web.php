d68441752e2fb3aaad20cc6b13362e89176d3b0e
Roman <r.sharapov@stream-telecom.ru>
2026-03-16 18:14:22 +0300

    feat(routes): add service route

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ProductsController::class, 'index'])->name('home');
 Route::get('/catalog', [ProductsController::class, 'getProducts'])->name('catalog');
 Route::get('/product/{id}', [ProductsController::class, 'getProductById'])->name('product');
-
-Route::get('/about', function () {
-    return 'about';
-});
-
+Route::view('/service', 'service')->name('service');
