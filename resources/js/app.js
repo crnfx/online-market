8617eec4629e93ee455086f74f6fce90c987dba7
Roman <r.sharapov@stream-telecom.ru>
2026-03-16 18:14:03 +0300

    feat(serviceTable): add table initialization

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,9 @@ import 'swiper/css';
 import 'swiper/css/effect-fade';
 
 import {initSwiper} from './components/swiper';
+import {initServiceTables} from "./components/serviceTable.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   initSwiper('.swiper');
+  initServiceTables();
 });
-
-console.log('assa')
-
