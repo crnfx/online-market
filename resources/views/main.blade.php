ed9c818efdde23371ee03515d6e76e0fde11a8e5
Roman <r.sharapov@stream-telecom.ru>
2026-03-16 13:23:49 +0300

    feat(main): update popular products

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,7 +1,69 @@
 @extends('layout.layout')
-@section('title', 'Почтовые рассылки')
+
+@section('title', 'Главная')
+
 @section('content')
   <section class="hero">
-    <img class="hero__image" src="{{ asset('images/image.avif') }}" alt="picture">
+    @component('components.swiper')
+    @endcomponent
+  </section>
+
+  <section class="popular">
+    <h2>Популярные товары</h2>
+
+    @if($popularProducts->isNotEmpty())
+      <div class="products-grid">
+        @foreach($popularProducts as $product)
+          <article class="product-card">
+            @if($product->sales_count > 10)
+              <span class="product-card__badge">Хит продаж</span>
+            @endif
+
+            <h3 class="product-card__title">{{ $product->name }}</h3>
+
+            <p class="product-card__description">
+              {{ Str::limit($product->description, 100) }}
+            </p>
+
+            <div class="product-card__prices">
+              @if(!is_null($product->sale_price))
+                <p class="product-card__price product-card__price--old">
+                  {{ number_format((float) $product->price, 0, '.', ' ') }} ₽
+                </p>
+                <p class="product-card__sale-price">
+                  {{ number_format((float) $product->sale_price, 0, '.', ' ') }} ₽
+                </p>
+              @else
+                <p class="product-card__price">
+                  {{ number_format((float) $product->price, 0, '.', ' ') }} ₽
+                </p>
+              @endif
+            </div>
+
+            <p class="product-card__quantity">
+              @if($product->quantity > 0)
+                В наличии: {{ $product->quantity }}
+              @else
+                <span class="out-of-stock">Нет в наличии</span>
+              @endif
+            </p>
+
+            <p class="product-card__sales">
+              Купили: {{ $product->sales_count }} раз
+            </p>
+
+            <a class="product-card__link" href="{{ route('product', $product->id) }}">
+              Подробнее
+            </a>
+          </article>
+        @endforeach
+      </div>
+    @else
+      <p class="empty-message">Пока нет популярных товаров.</p>
+    @endif
+  </section>
+
+  <section class="catalog-link">
+    <a href="{{ route('catalog') }}" class="btn btn--primary">Смотреть все товары</a>
   </section>
 @endsection
