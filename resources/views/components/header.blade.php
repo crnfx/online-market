2d241cde340876adcd209be6aa6c8271c881bf2d
Roman <r.sharapov@stream-telecom.ru>
2026-03-16 18:04:54 +0300

    feat(header): update header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,10 +4,27 @@
   </a>
   <ul class="header__list">
     <li class="header__item"><a class="header__link" href="{{ route('catalog')  }}">Каталог</a></li>
-    <li class="header__item"><a class="header__link" href="#">Услуги</a></li>
+    <li class="header__item"><a class="header__link" href="{{ route('service') }}">Услуги</a></li>
     <li class="header__item"><a class="header__link" href="#">Акции</a></li>
     <li class="header__item"><a class="header__link" href="#">О нас</a></li>
     <li class="header__item"><a class="header__link" href="#">Контакты</a></li>
   </ul>
-  <input name="search" class="header__search" type="text">
+  <form class="header__search-form" method="GET" action="{{ route('catalog') }}" role="search">
+    <label for="header-search" class="visually-hidden">Поиск товаров</label>
+    <input
+      id="header-search"
+      name="search"
+      class="header__search-input"
+      type="search"
+      placeholder="Введите название товара"
+      value="{{ request('search') }}"
+      aria-label="Поиск товаров"
+    >
+    <button class="header__search-btn" type="submit" aria-label="Найти">
+      <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+        <circle cx="11" cy="11" r="8"></circle>
+        <path d="m21 21-4.35-4.35"></path>
+      </svg>
+    </button>
+  </form>
 </header>
