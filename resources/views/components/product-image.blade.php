b6984b6a4f69831abbeb9ee96ee8a02d298ff139
Roman <r.sharapov@stream-telecom.ru>
2026-03-17 11:00:03 +0300

    feat(component): add product image swiper component

---
+++ resources/views/components/product-image.blade.php
@@ -0,0 +1,20 @@
+<div class="product__image">
+  <ul class="image-switch">
+    <li class="image-switch__item">
+      <div class="image-switch__img">
+        <img src="{{ asset('images/employer.webp') }}" alt="">
+      </div>
+    </li>
+    <li class="image-switch__item">
+      <div class="image-switch__img">
+        <img src="{{ asset('images/family.webp') }}" alt="">
+      </div>
+    </li>
+    <li class="image-switch__item">
+      <div class="image-switch__img">
+        <img src="{{ asset('images/girl.webp') }}" alt="">
+      </div>
+    </li>
+  </ul>
+  <ul class="product__image-pagination image-pagination" aria-hidden="true"></ul>
+</div>
