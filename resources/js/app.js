8599498b2f934d5c8ea5d2c70304f947152f2002
Roman <r.sharapov@stream-telecom.ru>
2026-03-17 11:00:30 +0300

    feat(components):  update components

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import 'swiper/css/effect-fade';
 
 import {initSwiper} from './components/swiper';
 import {initServiceTables} from "./components/serviceTable.js";
+import './components/productImagesSwiper';
 
 document.addEventListener('DOMContentLoaded', () => {
   initSwiper('.swiper');
