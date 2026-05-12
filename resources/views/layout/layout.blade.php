209e90475868adea234f1d50d34234fd6f9fe68f
Roman <r.sharapov@stream-telecom.ru>
2026-05-12 16:18:37 +0300

    feat(layout): add token to head and stacked styles

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -7,9 +7,11 @@
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="theme-color" content="#111111">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title')</title>
   @vite(['resources/sass/app.scss', 'resources/js/app.js'])
   @stack('link')
+  @stack('styles')
 </head>
 
 <body class="body">
