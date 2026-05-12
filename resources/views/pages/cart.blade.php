530cd8074a6ebaced5cf991cac5913940d162b63
Roman <r.sharapov@stream-telecom.ru>
2026-05-12 18:36:34 +0300

    feat(cart): add js

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'Корзина')
 
+@push('scripts')
+  @vite(['resources/js/components/cart.js'])
+@endpush
+
 @section('content')
   <section class="cart-page">
     <h1 class="cart-page__title">Корзина товаров</h1>
